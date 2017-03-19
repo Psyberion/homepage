5e46646ad3030eaea93b55ca6b4136b05c097b70
Erik <ebz@bth.se>
2017-03-19 22:17:35 +0100

    Update link config

--- src/js/colors.js
+++ src/js/colors.js
@@ -1,8 +1,1 @@
-/*
- * Defines what colours the background of the headers of each link-block
- * should have. The colours are chosen using `colors[i%colors.length]`,
- * so the array can be of arbitrary length
- */
-colors = [
-		"darkgrey",
-		];
+/home/ebz/.themes/cyberpunk/colors.js
